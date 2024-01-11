23de4ffae7c9ea0ee0feec253e041fde01318f5b
raghavoo <155086740+raghavoo@users.noreply.github.com>
2024-01-11 22:05:23 +0530

    Update myinfo.py

--- MukeshRobot/modules/myinfo.py
+++ MukeshRobot/modules/myinfo.py
@@ -11,11 +11,11 @@ from MukeshRobot.events import register
 
 edit_time = 5
 f""" ======================={BOT_NAME}====================== """
-file1 = "https://telegra.ph/file/9a85d0a873e2dd80d278d.jpg"
-file2 = "https://telegra.ph/file/9e7815284031452afa9e5.jpg"
-file3 = "https://telegra.ph/file/dcc5e003287f69acea368.jpg"
-file4 = "https://telegra.ph/file/ed1ce7fee94f46b0f671e.jpg"
-file5 = "https://telegra.ph/file/701028ce085ecfa961a36.jpg"
+file1 = "https://te.legra.ph/file/88977fae533165fa9a3f6.jpg"
+file2 = "https://te.legra.ph/file/75be49dd237fbbb536fe5.jpg"
+file3 = "https://te.legra.ph/file/c2216b55b5184c6c82bd2.jpg"
+file4 = "https://te.legra.ph/file/ce9449ba241a800be90ff.jpg"
+file5 = "https://te.legra.ph/file/6140e420212ecc8a5eb5d.jpg"
 """ ======================={BOT_NAME}====================== """
 
 
