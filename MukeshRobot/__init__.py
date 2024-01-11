6e8facb44b171433b814e59b0088238ab31b42b1
raghavoo <155086740+raghavoo@users.noreply.github.com>
2024-01-11 21:14:54 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -52,14 +52,14 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "the_friendz")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "raghavsupport")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     WORKERS = int(os.environ.get("WORKERS", 8))
 
     try:
-        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+        OWNER_ID = int(os.environ.get("OWNER_ID", 6156380294))
     except ValueError:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
